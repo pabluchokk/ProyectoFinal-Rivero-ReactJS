994140e797b1fbfcb5312ccdbde7e01639059ade
pabluchokk <pabloagusrivero17@gmail.com>
2023-12-06 18:23:14 -0300

    Form
    Utilizando la librería react-hook-form

--- src/components/Contacto.jsx
+++ src/components/Contacto.jsx
@@ -1,60 +1,29 @@
-import { useState } from "react";
+import { useForm } from "react-hook-form";
 
 const Contacto = () => {
 
-    const [valores, setValores] = useState({
-        nombre: "",
-        email: ""
-    })
+    const { register, handleSubmit } = useForm()
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        console.log("Enviado", {valores})
-    }
-
-    const handleValores = (e) => {
-        console.log(e.target.name)
-        setValores({
-            ...valores,
-            [e.target.name]: e.target.value
-        })
+    const enviar = (data) => {
+        console.log(data)
     }
 
     return (
+
         <div className="container">
             <h1 className="main-title">Contacto</h1>
-            <form className="formulario" onSubmit={handleSubmit}>
-                <input 
-                    type="text" 
-                    placeholder="Ingresá tu nombre..."
-                    value={valores.nombre}
-                    onChange={handleValores}
-                    name="nombre"
-                />
+            <form className="formulario" onSubmit={handleSubmit(enviar)}>
 
-                <input 
-                    type="email" 
-                    placeholder="Ingresá tu email..."
-                    value={valores.email}
-                    onChange={handleValores}
-                    name="email"
-                />
+                <input type="text" placeholder="Ingresá tu nombre..." {...register("nombre")}/>
 
-                <input 
-                    type="phone" 
-                    placeholder="Ingresá tu teléfono..."
-                    value={valores.telefono}
-                    onChange={handleValores}
-                    name="telefono"
-                />
+                <input type="email" placeholder="Ingresá tu email..." {...register("email")}/>
+                
+                <input type="phone" placeholder="Ingresá tu teléfono..." {...register("telefono")}/>
 
-                <button 
-                    type="submit" 
-                    className="enviar">Enviar
-                </button>
+                <button type="submit" className="enviar">Enviar</button>
             </form>
         </div>
-    )
-}
+    );
+};
 
-export default Contacto
+export default Contacto;
